564b100cfcac19a5e7e82ff5f25adfd0cda5023d
Riccardo Iaconelli <riccardo@kde.org>
2013-05-19 15:23:35 +0200

    update
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- Neverland.php
+++ Neverland.php
@@ -206,7 +206,7 @@ class NeverlandTemplate extends BaseTemplate {
             <!-- subtitle -->
             <div id="contentSub"<?php $this->html( 'userlangattributes' ) ?>>
               <ul class="breadcrumb">
-              <?php print $wgOut->getSubtitle(); ?>
+              <?php print $this->text('subtitle'); ?>
                 <li class="active"><?php $this->html( 'subtitle' ) ?></li>
               </ul>
             </div>

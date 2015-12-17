c791b015a72536712b0c8dbab4ff3d8b407c7f19
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2015-12-17 22:44:00 +0100

    Displays login instead of user ip

--- Neverland.php
+++ Neverland.php
@@ -66,7 +66,7 @@ class NeverlandTemplate extends BaseTemplate {
    */
   public function execute() {
     global $wgRequest, $wgOut, $wgCanonicalNamespace, $wgContLang, $wgSitename,
-         $wgLogo, $wgStylePath, $wgNeverlandUseIconWatch, $wiki_domain;
+         $wgLogo, $wgStylePath, $wgNeverlandUseIconWatch, $wiki_domain, $wgUser;
 
     $skin = $this->getSkin();
     $user = $skin->getUser();
@@ -207,7 +207,8 @@ class NeverlandTemplate extends BaseTemplate {
 
                 <ul class="nav navbar-nav navbar-right">
                     <li id="userlogin" class="dropdown col-xs-6 col-lg-10 col-md-10 col-sm-10">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="user-login" style="min-width:100px;"> <?php echo $user->getName(); ?> <span id="userlogin_caret" class="caret"></span></a>
+                      <?php $log_in = ($wgUser->isAnon()) ? "Login" : $user->getName() ; ?>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="user-login" style="min-width:100px;"> <?php echo $log_in; ?> <span id="userlogin_caret" class="caret"></span></a>
                       <ul class="dropdown-menu">
                         <?php
                                 // generate user tools (and notifications item in user tools if needed)
